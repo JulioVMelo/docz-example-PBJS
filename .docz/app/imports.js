f9c90b399023e4380cab814e43ae6a9456f33cbb
Julio Melo <julio@alfamultimidia.com.br>
2019-07-11 15:15:49 -0300

    atualizando repo

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,4 +3,8 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-hello" */ 'src/hello.mdx'
     ),
+  'src/components/button/button.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-button-button" */ 'src/components/button/button.mdx'
+    ),
 }
