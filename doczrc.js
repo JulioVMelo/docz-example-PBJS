f2901dc06d5472e29cb14dfbaaa6f34c4c180927
Julio Melo <julio@alfamultimidia.com.br>
2019-07-11 23:04:18 -0300

    theme config

---
+++ doczrc.js
@@ -0,0 +1,12 @@
+// doczrc.js
+
+export default {
+  title: 'Paraiba JS',
+  description: 'This is my awesome documentation',
+  themeConfig: {
+    mode: 'dark',
+    colors: {
+      primary: '#62D463',
+    },
+  },
+}
